41d810c174e065cf085e08b949ffaab8b70638a5
4N70N1U5 <nitoi.antonio@gmail.com>
2023-01-22 01:58:06 +0200

    Added User model.

--- ProiectDAW/Data/DataContext.cs
+++ ProiectDAW/Data/DataContext.cs
@@ -1,7 +1,11 @@
-﻿namespace ProiectDAW.Data
+﻿using ProiectDAW.Models;
+
+namespace ProiectDAW.Data
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ ProiectDAW/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using ProiectDAW.Models.Base;
+
+namespace ProiectDAW.Models
+{
+    public class User : BaseEntity
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
