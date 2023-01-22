4429ad29234b071095922cc7434dd234357e948c
4N70N1U5 <nitoi.antonio@gmail.com>
2023-01-22 04:02:32 +0200

    Added VideoGames and OrderVideoGame tables migration.

---
+++ ProiectDAW/Migrations/20230122015608_VideoGamesCreate.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProiectDAW.Migrations
+{
+    /// <inheritdoc />
+    public partial class VideoGamesCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VideoGames",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    PublisherId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoGames", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VideoGames_Publishers_PublisherId",
+                        column: x => x.PublisherId,
+                        principalTable: "Publishers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderVideoGame",
+                columns: table => new
+                {
+                    OrdersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    VideoGamesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderVideoGame", x => new { x.OrdersId, x.VideoGamesId });
+                    table.ForeignKey(
+                        name: "FK_OrderVideoGame_Orders_OrdersId",
+                        column: x => x.OrdersId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderVideoGame_VideoGames_VideoGamesId",
+                        column: x => x.VideoGamesId,
+                        principalTable: "VideoGames",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderVideoGame_VideoGamesId",
+                table: "OrderVideoGame",
+                column: "VideoGamesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VideoGames_PublisherId",
+                table: "VideoGames",
+                column: "PublisherId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderVideoGame");
+
+            migrationBuilder.DropTable(
+                name: "VideoGames");
+        }
+    }
+}
