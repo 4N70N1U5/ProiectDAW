9a68037dd7d214bb90a4c22af1c0cc4e7d73fa3e
4N70N1U5 <nitoi.antonio@gmail.com>
2023-01-22 17:19:06 +0200

    Added GenericRepository.

---
+++ ProiectDAW/Repositories/GenericRepository/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Data.SqlClient;
+using ProiectDAW.Models.Base;
+
+namespace ProiectDAW.Repositories.GenericRepository
+{
+    public class GenericRepository<TemplateEntity> : IGenericRepository<TemplateEntity> where TemplateEntity : BaseEntity
+    {
+        private readonly DataContext _context;
+        private readonly DbSet<TemplateEntity> _table;
+
+        public GenericRepository(DataContext context)
+        {
+            _context = context;
+            _table = _context.Set<TemplateEntity>();
+        }
+
+        // Create
+        public async Task CreateAsync(TemplateEntity templateEntity)
+        {
+            await _table.AddAsync(templateEntity);
+        }
+
+        // Read
+        public async Task<List<TemplateEntity>> GetAllAsync()
+        {
+            return await _table.ToListAsync();
+        }
+
+        public async Task<TemplateEntity> GetByIdAsync(Guid id)
+        {
+            return await _table.FindAsync(id);
+        }
+
+        // Update
+        public void Update(TemplateEntity templateEntity)
+        {
+            _table.Update(templateEntity);
+        }
+
+        // Delete
+        public void Delete(TemplateEntity templateEntity)
+        {
+            _table.Remove(templateEntity);
+        }
+
+        // Save
+        public async Task<bool> SaveAsync()
+        {
+            try
+            {
+                return await _context.SaveChangesAsync() > 0;
+            }
+            catch (SqlException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ProiectDAW/Repositories/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using ProiectDAW.Models.Base;
+
+namespace ProiectDAW.Repositories.GenericRepository
+{
+    public interface IGenericRepository<TemplateEntity> where TemplateEntity : BaseEntity
+    {
+        // Create
+        Task CreateAsync(TemplateEntity templateEntity);
+
+        // Read
+        Task<List<TemplateEntity>> GetAllAsync();
+        Task<TemplateEntity> GetByIdAsync(Guid id);
+
+        // Update
+        void Update(TemplateEntity templateEntity);
+
+        // Delete
+        void Delete(TemplateEntity templateEntity);
+
+        // Save
+        Task<bool> SaveAsync();
+    }
+}
