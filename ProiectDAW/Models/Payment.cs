1f36ad9405884c91789941db9fad912a695177ea
4N70N1U5 <nitoi.antonio@gmail.com>
2023-01-22 02:42:28 +0200

    Updated Payment model.

--- ProiectDAW/Models/Payment.cs
+++ ProiectDAW/Models/Payment.cs
@@ -4,6 +4,8 @@ namespace ProiectDAW.Models
 {
     public class Payment : BaseEntity
     {
-        public string CardIssuer { get; set; } = string.Empty;
+        public string CardIssuer { get; set; } = string.Empty; // Visa or Mastercard.
+        public string CardNumber { get; set; } = string.Empty; // Last 4 digits of card number.
+        public string CardType { get; set; } = string.Empty; // Debit or credit card.
     }
 }
