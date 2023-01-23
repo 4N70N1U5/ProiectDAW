068d7e55c79843f6590a3ba99719d915e5b35cc8
4N70N1U5 <nitoi.antonio@gmail.com>
2023-01-23 14:21:21 +0200

    Updated UserRequestDTO (first and last name optional).

--- ProiectDAW/Models/DTOs/UserDTOs/UserRequestDTO.cs
+++ ProiectDAW/Models/DTOs/UserDTOs/UserRequestDTO.cs
@@ -4,8 +4,8 @@ namespace ProiectDAW.Models.DTOs.UserDTOs
 {
     public class UserRequestDTO
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
         [Required] public string Username { get; set; }
         [Required] public string Email { get; set;}
         [Required] public string Password { get; set; }
